5e10d2ceb4acb6f2b51f3331ec18867193879553
lizhongmin <lizhongmin@sinoiov.com>
2020-07-16 17:09:12 +0800

    [feat] 基础配置测试

---
+++ generator.js
@@ -0,0 +1,49 @@
+/**
+ * api : 一个 GeneratorAPI 实例
+ * options: 可以先简单理解为 prompts 问题数组的用户输入 组合成的选项对象
+ * rootOptions: 整个 preset.json 对象
+ */
+module.exports = (api, options, rootOptions) => {
+  // options.project 可以访问上面问题数组的第一个对象的值，默认为: 'vue-web'
+  console.log(`Your choice is ${options.project}`);
+  console.log(`Your input is ${options.projectName}`);
+
+  // 1.判断控制台用户的输入
+  if (options.project === "vue-web") {
+    // render函数把该路径下的 ./template/vue-web1 文件拷贝到默认的vue项目中。
+    // 如果文件的路径和文件名称相同的则覆盖，否则是叠加
+    api.render("./template/vue-web1", { InputOptions: { ...options } });
+  }
+
+  if (options.project === "base-web") {
+    // 2.拷贝文件并传递变量。例如：InputOptions对象中的属性/变量会覆盖掉该./template/base-web2路劲下html,js,json等文件对应的属性/变量
+    api.render("./template/base-web2", { InputOptions: { ...options } });
+  }
+
+  if (options.project === "pc-web") {
+    // 3.如果该./template/pc-web3 路径下的内容为空，会使用vue-cli中提供的默认的模板
+    api.render("./template/pc-web3", { InputOptions: { ...options } });
+  }
+
+  if (options.project === "mob-web") {
+    // 4.如果该 render 没有执行，会使用 vue-cli 中提供的默认的模板
+    // api.render('./template/mob-web4', { InputOptions:{ ...options} })
+  }
+
+  // 3.修改 `package.json` 里的字段
+  api.extendPackage({
+    // 4.添加第三库的依赖
+    dependencies: {
+      // 'normalize.css': '^8.0.1'
+    },
+    // 4.添加第三库的依赖
+    devDependencies: {
+      // 'normalize.css': '^8.0.1'
+    },
+    // 5.添加自定义的脚本
+    scripts: {
+      // 'dev': 'vue-cli-service serve'
+    },
+    config: {},
+  });
+};
---
+++ prompts.js
@@ -0,0 +1,32 @@
+// let chalk = require('chalk')
+/**
+ * 本质上是一个对话配置文件
+ */
+module.exports = [
+  {
+    type: "list", // 即类型为 选择项
+    name: "project", // 名称，作为下面 generator.js导出 函数 options 的键
+    message: "请选择你要生成的项目类型", // 提示语
+    choices: [
+      { name: "vue-web", value: "vue-web" },
+      { name: "base-web", value: "base-web" },
+      { name: "pc-web", value: "pc-web" },
+      { name: "mo-web", value: "mo-web" },
+    ],
+    default: "vue-web",
+  },
+  {
+    type: "input", // 类型为 输入项
+    name: "projectName", // 名称，作为下面 generator.js导出 函数 options 的键
+    message: "请输入项目的名称", // 提示语
+    default: "vue-web",
+  },
+  // {
+  //   type: 'confirm',
+  //   name: 'testConfirm',
+  //   // 该项目唯一安装的依赖 chalk; 目的是使用控制台输出 chalk.yellow('xxx') xxx 文字有颜色
+  //   // ? Use history mode for router? (Requires proper server setup for index fallback in production) (Y/n)
+  //   message: `Use history mode for router? ${chalk.yellow(`(Requires proper server setup for index fallback in production)`)}`,
+  //   description: `Using History API, the URLs don't need  '#' character.`
+  // }
+];
